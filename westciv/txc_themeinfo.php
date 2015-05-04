bfd74f679f90aa778655dfbe9fb7d747af130b1e
mambax7 <mambax7@gmail.com>
2015-05-04 05:50:39 -0400

    Original files

---
+++ westciv/txc_themeinfo.php
@@ -0,0 +1,6 @@
+<?php
+	$txc_themeinfo["name"] = "westciv";
+	$txc_themeinfo["author"] = "westciv";
+	$txc_themeinfo["author_url"] = "http://www.westciv.com/";
+	$txc_themeinfo["download_url"] = "http://xoops.org.cn/modules/wfdownloads/visit.php?cid=6&lid=475";
+?>
